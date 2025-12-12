5cafd86529f52ea4dc3bd192f832a15ca162389f
ahmedfyaz <awanahmed755@gmail.com>
2025-12-13 01:09:06 +0500

    chap 3 completed

--- 03-props/src/App.jsx
+++ 03-props/src/App.jsx
@@ -3,13 +3,8 @@ import Cards from "./components/cards.jsx";
 const App = () => {
     return (
         <div className={'parent'}>
-            <Cards/>
-            <Cards/>
-            <Cards/>
-            <Cards/>
-            <Cards/>
-            <Cards/>
-
+            <Cards user={'Ahmed'} age={19} image={"https://images.unsplash.com/photo-1762751358910-345cde48bf9c?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}/>
+            <Cards user={'Masfa'} age={18}  image={"https://images.unsplash.com/photo-1753847726685-6af24062aa9e?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}/>
         </div>
     )
 }
--- 03-props/src/components/cards.jsx
+++ 03-props/src/components/cards.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
-const Cards = () => {
+const Cards = (props) => {
     return (
         <div className={'card'}>
-            <img src="https://images.unsplash.com/photo-1762751358910-345cde48bf9c?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt={'no'}/>
+            <img src={props.image} alt={'no'}/>
             <h1>
-                Ahmed Awan
+                {props.user}
             </h1>
             <p>
-                lorem ipsum dolor sit amet
+                {props.age}
             </p>
             <button>View Profile</button>
         </div>
