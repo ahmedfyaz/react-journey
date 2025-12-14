eb8eaa738c6e82c0c1ebf319adf0c4ecc48ecde3
ahmedfyaz <awanahmed755@gmail.com>
2025-12-14 14:45:20 +0500

    chap 6 intialized

---
+++ 05-css/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Buttons from "./components/buttons/Buttons.jsx";
+import Header from "./components/headers/Header.jsx";
+
+const App = () => {
+    return (
+        <div>
+            <Header/>
+            <Buttons/>
+        </div>
+    )
+}
+export default App
