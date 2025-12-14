4b6d0f41192eb72ffd092b79aafe5013cac78060
ahmedfyaz <awanahmed755@gmail.com>
2025-12-14 12:31:53 +0500

    started passing procs

--- 04-cards-project/src/App.jsx
+++ 04-cards-project/src/App.jsx
@@ -2,16 +2,27 @@ import React from 'react'
 import Card from "./components/card.jsx";
 const App = () => {
     const arr =
-        [
+            [
             {
-        company:"Amazon"
-    },{company: "fayyaz"},{company: "awan"}]
-    return (
-        <div className={"parent"}>
-            {arr.map(function (elem) {
-                return <Card company={elem.company}/>
-            })}
-        </div>
+                image:"https://imgs.search.brave.com/1SmDks4CPsuEM1txgXrUz2cuSfM8XLjvrvTIacq4V2s/rs:fit:32:32:1:0/g:ce/aHR0cDovL2Zhdmlj/b25zLnNlYXJjaC5i/cmF2ZS5jb20vaWNv/bnMvODYwYzA4NzE1/OTE2NzM1YTQ1OWNj/Zjg0MmI0Y2U2YzI2/ODE5NmZhNGZjODA4/N2Y0NmI1ZmU4ZWQy/ZmM2MjdmMS93d3cu/YW1hem9uLmNvbS8",
+                company:"Amazon",
+                price:"12",
+                days:"5",
+                job_type:"senior",
+                location:"Faisalabad,Pakistan"
+            },
+            {
+                company: "fayyaz"},
+            {
+                company: "awan"
+            }
+            ]
+            return (
+            <div className={"parent"}>
+                {arr.map(function (elem) {
+                    return <Card company={elem.company}/>
+                })}
+            </div>
     )
 }
 export default App
