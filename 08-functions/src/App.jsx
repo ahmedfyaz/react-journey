e8ef721753678b7b783f4d31d6d0576fd975280d
ahmedfyaz <awanahmed755@gmail.com>
2025-12-16 23:41:07 +0500

    started learning functions

--- 08-functions/src/App.jsx
+++ 08-functions/src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
 const App = () => {
+    function btnClicked() {
+        console.log("Button Clicked")
+    }
+    function mouseEntered(){
+        console.log("Mouse Entered")
+    }
     return (
-        <div>Hello world</div>
+        <div>
+            <h1>Hello world</h1>
+            <button onMouseEnter={mouseEntered} onDoubleClick={btnClicked}> FUCK You</button>
+        </div>
     )
 }
 export default App
