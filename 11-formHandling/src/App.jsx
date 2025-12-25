fd562a9edd835028af5beb10ce173a18bca4b868
ahmedfyaz <awanahmed755@gmail.com>
2025-12-25 11:17:54 +0500

    Chap 11 completed

--- 11-formHandling/src/App.jsx
+++ 11-formHandling/src/App.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import {useState} from "react";
 
 const App = () => {
-    const submitHandler = () => {
-        console.log('submitHandler')
+
+    const [value, setValue] = useState('')
+    const submitHandler = (e) => {
+        e.preventDefault()
+        console.log('submitHandler', value)
+        setValue('')
     }
     return (
         <div>
-            <form onSubmit={submitHandler}>
-                <input type={'text'} placeholder={'Enter your name'}/>
+            <form onSubmit={(e)=>{
+                submitHandler(e)
+            }}>
+                <input type={'text'} placeholder={'Enter your name'}
+                value={value}
+                onChange={(e)=>
+                {
+                    setValue(e.target.value)
+                    console.log(value)
+                }}/>
                 <button>Submit</button>
             </form>
         </div>
