343c767cebc044e39ce69dec7bcae0d0ce2b5763
ahmedfyaz <awanahmed755@gmail.com>
2025-12-26 20:29:16 +0500

    project completed

--- 12-notes-project/src/App.jsx
+++ 12-notes-project/src/App.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import { X } from 'lucide-react';
 const App = () => {
 
     const [title, setTitle] = useState('');
@@ -16,6 +17,13 @@ const App = () => {
 
         setTitle('')
         setDetail('')
+    }
+
+    const deleteNote = (idx)=>
+    {
+        const copyTask = [...task]
+        copyTask.splice(idx,1)
+        setTask(copyTask)
     }
 
 
@@ -47,11 +55,21 @@ const App = () => {
                 </form>
                 <div className={'lg:w-1/2 lg:border-l-2 p-10'}>
                     <h1 className={'text-3xl font-bold'}>Your Notes</h1>
-                        <div className={'flex gap-5 mt-5 flex-wrap h-full overflow-auto '}>
+                        <div className={'flex gap-5 mt-5 flex-wrap items-start justify-start h-full overflow-auto '}>
                             {task.map((function(e ,idx){
-                                return <div key={idx} className={'h-52 w-40 text-black rounded-2xl bg-white'}>
-                                    <h3 className={'leading-tight text-xl font-bold'}>{e.title}</h3>
-                                    <p className={'mt-2 leading-tight font-medium text-gray-800'}>{e.detail}</p>
+                                return <div key={idx}  className={"flex justify-between items-start flex-col relative h-52 w-40 pt-9 pb-4 px-4 text-black rounded-xl bg-cover bg-[url('https://imgs.search.brave.com/b9nc5BMwJ963hopkarex8iyCPZDL5oVweOdhvT2icjI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wMjQv/NTg0LzQ2Mi9zbWFs/bC9ibGFuay1zcGFj/ZS13aGl0ZS1zdGlj/a3ktbm90ZS1wbmcu/cG5n')]"}>
+                                    <div>
+                                        <h3 className={'leading-tight text-xl font-bold'}>{e.title}</h3>
+                                        <p className={'mt-2 leading-tight text-xs text-gray-800'}>{e.detail}</p>
+                                    </div>
+                                    <button onClick={()=>
+                                    {
+                                        deleteNote(idx)
+                                    }} className={'w-full cursor-pointer active:scale-95 bg-red-500 py-1 text-xs rounded font-bold text-white'
+                                        }>
+                                        Delete
+                                    </button>
+
                                 </div>
                             }))}
                         </div>
