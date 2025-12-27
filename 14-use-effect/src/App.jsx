da7df1f9851b6f971ea572a8c9201174f5fe165f
ahmedfyaz <awanahmed755@gmail.com>
2025-12-27 22:49:52 +0500

    chapter 15 started

--- 14-use-effect/src/App.jsx
+++ 14-use-effect/src/App.jsx
@@ -1,12 +1,25 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 
 const App = () => {
 
+    const [num, setNum] = useState(0)
+    const [num2, setNum2] = useState(100)
     useEffect(() => {
         console.log('render');
-    });
+    },[num]);
     return (
-        <div>App</div>
+        <div>
+            <h1>{num}</h1>
+            <h1>{num2}</h1>
+            <button onClick={()=>
+            {
+                setNum(num+1)
+            }}
+            onMouseEnter={()=>
+            {
+                setNum2(num2+10)
+            }}>Click me</button>
+        </div>
     )
 }
 export default App
